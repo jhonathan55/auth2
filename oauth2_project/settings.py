ed9e91a0e669409ecf6b338f192066f18183dd88
jhonathan55 <jfgutierrezsaico@gmail.com>
2026-03-27 19:42:33 -0300

    cors

--- oauth2_project/settings.py
+++ oauth2_project/settings.py
@@ -38,13 +38,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'rest_framework',
     'OAuth2',
 ]
 # Custom user model
 AUTH_USER_MODEL = "OAuth2.AppUser"
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
